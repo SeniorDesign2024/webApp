c074e6672060c26f3153adb5994867aee9a4d68f
Ankit Mistry <amistry2@vols.utk.edu>
2024-02-05 20:37:25 -0500

    tailwindcss setup

---
+++ react-web-app/tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}"],
+  mode: 'jit',
+  theme: {
+    extend: {
+      colors: {
+        black: "#000000",
+        orange: "#FF7F50",
+        white: "#FFFFFF",
+        "dark-purple": "#3A0CA3",
+        "light-purple": "#E7CDE1",
+        "cream-white": "#F8F6F3",
+      },
+      fontFamily: {
+        playfair: ["Playfair Display", "serif"],
+        opensans: ["Open Sans", "sans-serif"]
+      }
+    },
+    screens: {
+      xs: "480px",
+      sm: "768px",
+      md: "1060px",
+    },
+  },
+  plugins: [],
+}
+
